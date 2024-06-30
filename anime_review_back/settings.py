6be811fe8c3a97c2833cf78cf5f136c4d2083c33
yoonju1030 <allrealjgt@gmail.com>
2024-07-01 02:51:03 +0900

    make requirement

--- anime_review_back/settings.py
+++ anime_review_back/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
-CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:8080','http://localhost:8080']
+CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:8080','http://localhost:8080', "http://35.208.129.216:8080"]
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = (
