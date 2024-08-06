06e66b30c439dd5b0a4911ef6c12d40556200b0e
yoonju1030 <allrealjgt@gmail.com>
2024-08-07 00:14:48 +0900

    modify os environ type

--- utils/config.py
+++ utils/config.py
@@ -5,11 +5,11 @@ class Config(object):
     DEBUG = False
     TESTING = False
     # MongoDB Info
-    MONGODB_USERNAME = os.environ["MONGODB_USERNAME"]
-    MONGODB_PASSWORD = os.environ["MONGODB_PASSWORD"]
-    MONGODB_DATABASE = os.environ["MONGODB_DATABASE"]
-    MONGODB_HOST = os.environ["MONGODB_HOST"]
-    MONGODB_PORT = os.environ["MONGODB_PORT"]
-    ALLOWED_HOST = os.environ["ALLOWED_HOST"]
+    MONGODB_USERNAME = os.environ.get("MONGODB_USERNAME")
+    MONGODB_PASSWORD = os.environ.get("MONGODB_PASSWORD")
+    MONGODB_DATABASE = os.environ.get("MONGODB_DATABASE")
+    MONGODB_HOST = os.environ.get("MONGODB_HOST")
+    MONGODB_PORT = os.environ.get("MONGODB_PORT")
+    ALLOWED_HOST = os.environ.get("ALLOWED_HOST")
 
 config = {"config": Config}
