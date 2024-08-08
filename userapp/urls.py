c0c8e9defe9bbf3d65888cda0d711748fba1898a
yoonju1030 <allrealjgt@gmail.com>
2024-08-08 21:53:29 +0900

    create function to login with jwt

--- userapp/urls.py
+++ userapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from userapp.views import check_unique_id, sign_up_user, log_in_user, log_out_user
+from userapp.views import check_unique_id, sign_up_user, log_in_user, log_out_user, test
 
 urlpatterns = [
     path("check_id", check_unique_id),
     path("signup", sign_up_user),
     path("login", log_in_user),
-    path("logout", log_out_user)
+    path("logout", log_out_user),
+    path("test", test)
 ]
--- userapp/views.py
+++ userapp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from utils.mongo import MongoDB
 from datetime import datetime
 from .serialize import UserSerializer
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import status
+from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
 
 db = MongoDB()
 
@@ -24,7 +26,6 @@ def check_unique_id(request):
 @api_view(['POST'])
 def sign_up_user(request):
     try:
-
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
@@ -47,7 +48,6 @@ def sign_up_user(request):
 
             res.set_cookie("access", access_token, httponly=True)
             res.set_cookie("refresh", refresh_token, httponly=True)
-            
             return res
         else:
             raise e
@@ -57,10 +57,43 @@ def sign_up_user(request):
 @api_view(['POST'])
 def log_in_user(request):
     try:
-        pass
+        user = authenticate(
+            id=request.data.get("id"), password=request.data.get("password")
+        )
+        if user is not None:
+            serializer = UserSerializer(user)
+            # jwt 토큰 접근
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "login success",
+                    "token": {
+                        "access": access_token,
+                        "refresh": refresh_token,
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+            # jwt 토큰 => 쿠키에 저장
+            res.set_cookie("access", access_token, httponly=True)
+            res.set_cookie("refresh", refresh_token, httponly=True)
+            return res
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
         raise e
-    
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def test(request):
+    try:
+        return Response({"message": "success"})
+    except Exception as e:
+        raise e
+
 @api_view(["POST"])
 def log_out_user(request):
     try:
