a9fa484bb0f3a4f95a39ec2925405249d4daf390
yoonju1030 <allrealjgt@gmail.com>
2024-08-12 23:13:21 +0900

    add new function to get new access token with refresh token

--- userapp/urls.py
+++ userapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from userapp.views import check_unique_id, sign_up_user, log_in_user, log_out_user, test
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("check_id", check_unique_id),
     path("signup", sign_up_user),
     path("login", log_in_user),
     path("logout", log_out_user),
-    path("test", test)
+    path("test", test),
+    path('refresh', TokenRefreshView.as_view())
 ]
