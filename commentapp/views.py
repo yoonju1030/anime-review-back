9a3096c0b98e9c7666cfce7286c6fa641aba8abf
yoonju1030 <allrealjgt@gmail.com>
2024-08-16 00:11:56 +0900

    make function to create comment

--- commentapp/views.py
+++ commentapp/views.py
@@ -3,12 +3,21 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Comment
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def create_comment(request):
     try:
+        anime_id = request.data.get('animeId')
+        user_id = request.data.get("userId")
+        content = request.data.get('content')
+        Comment.objects.create(
+            user_id=user_id,
+            content=content,
+            anime=anime_id
+        )
         response = Response({"result": status.HTTP_200_OK})
         return response
     except Exception as e:
-        raise e
+        return Response({'message': "create comment fail", 'result': False})
