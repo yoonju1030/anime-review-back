3136eba14a2c24c679ef991cd92d3e91153bd679
yoonju1030 <allrealjgt@gmail.com>
2024-08-24 21:42:07 +0900

    make function to get comments by users

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from commentapp.views import create_comment, get_all_comment_by_anime
+from commentapp.views import create_comment, get_all_comment_by_anime, get_comment_by_users
 
 urlpatterns = [
     path('create', create_comment),
-    path('get_comments/', get_all_comment_by_anime)
+    path('get_comments/', get_all_comment_by_anime),
+    path('get_comment_by_users/', get_comment_by_users)
 ]
