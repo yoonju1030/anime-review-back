34e9b0ee75218cf3ac80e03d49180eceea892447
yoonju1030 <allrealjgt@gmail.com>
2024-08-28 17:30:10 +0900

    Change the animation title to appear in the comments as well

--- commentapp/views.py
+++ commentapp/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Comment
 from utils.mongo import MongoDB
+from bson import ObjectId
 
 db = MongoDB()
 
@@ -36,11 +37,19 @@ def get_all_comment_by_anime(request):
         return Response({'message': "Fail to get comment", 'result': False})
     
 @api_view(["POST"])
+@permission_classes([IsAuthenticated])
 def get_comment_by_users(request):
     try:
         user_id = request.data.get('userId')
         result = db.get_many_data(db.db, "commentapp_comment", {"user_id": user_id})
         comments = [{"content": a['content'], "id": a['anime'], "time": a['created_at']} for a in result]
+        anime_lists = list(set([ObjectId(c["id"]) for c in comments]))
+        anime_name_list = db.get_many_data(db.db, "anime", {"_id": {"$in":anime_lists}})
+        name_obj = {}
+        for anime in anime_name_list:
+            name_obj[str(anime["_id"])] = anime["name"]
+        for c in comments:        
+            c["name"] = name_obj[c["id"]]
         return Response({'message': "Success to get comments", 'result':comments})
     except Exception as e:
         return Response({'message': "Fail to get comment", 'result': False})
