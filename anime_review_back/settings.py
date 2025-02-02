f308d531aceb83ed381f3695d5fcffe190a6d15b
yoonju1030 <allrealjgt@gmail.com>
2025-02-02 17:43:06 +0900

    modify allowed url

--- anime_review_back/settings.py
+++ anime_review_back/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://35.208.129.216:8000",
+    "http://35.208.129.216:8080",
     'http://127.0.0.1:8080',
     'http://localhost:8080', 
     "http://35.209.196.239",
